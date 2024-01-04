f2da1cad67371f73e576c645fbf1c322eb32f825
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-04 16:49:01 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
 function App() {
 
     return (
         <>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+            </Routes>
         </>
     )
 }
