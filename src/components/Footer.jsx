7aa49612f8b51d1138268f75280aacce577bcce1
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-05 00:49:38 +0530

    Created footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-white dark:bg-black mt-auto">
+                <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center flex justify-center">
+            <span className="text-sm text-black text-center dark:text-white">
+                Made with &#x1F90D; by Nureka Rodrigo
+            </span>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
