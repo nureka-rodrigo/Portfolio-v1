fa29ce8f82e5131a1d911c87a1554adacc63d2eb
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-06 01:24:04 +0530

    Created contact me page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Projects from "./pages/Projects.jsx";
+import Resume from "./pages/Resume.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
-
     return (
         <>
             <Routes>
                 <Route path={"/"} element={<Home/>}/>
-                <Route path={"/projects"} element={<Projects/>}/>
+                <Route path={"projects"} element={<Projects/>}/>
+                <Route path={"resume"} element={<Resume/>}/>
+                <Route path={"contact"} element={<Contact/>}/>
             </Routes>
         </>
     )
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,79 @@
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+
+const Contact = () => {
+    return (
+        <>
+            <div className="flex flex-col min-h-screen min-w-fit">
+                <Header />
+                <section className="bg-white dark:bg-black">
+                    <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+                            Contact Me
+                        </h2>
+                        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+                            I welcome the opportunity to connect with you. Whether you have inquiries about my work, collaboration proposals, or just want to say hello, feel free to reach out. I am always eager to engage with new ideas and projects.
+                        </p>
+                        <form action="#" className="space-y-8">
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                >
+                                    Your email
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                    placeholder="email@abc.com"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="subject"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                                >
+                                    Subject
+                                </label>
+                                <input
+                                    type="text"
+                                    id="subject"
+                                    className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                                    placeholder="Let me know how I can help you"
+                                    required
+                                />
+                            </div>
+                            <div className="sm:col-span-2">
+                                <label
+                                    htmlFor="message"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                                >
+                                    Your message
+                                </label>
+                                <textarea
+                                    id="message"
+                                    rows="6"
+                                    className="block p-2.5 w-full text-sm text-gray-900 rounded-lg shadow-sm border border-gray-300 focus:ring-gray-500 focus:border-gray-300 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    placeholder="Leave a comment..."
+                                ></textarea>
+                            </div>
+                            <div className="mx-auto">
+                                <button
+                                    type="submit"
+                                    className="bg-black dark:bg-white text-white dark:text-gray-900 py-3 px-5 text-sm font-medium text-center rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                >
+                                    Send message
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+                <Footer />
+            </div>
+        </>
+    );
+};
+
+export default Contact;
