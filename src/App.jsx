10f456a89f288b89a5d33b91a4c6c8a717b51887
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-05 20:56:00 +0530

    Updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Projects from "./pages/Projects.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
         <>
             <Routes>
                 <Route path={"/"} element={<Home/>}/>
+                <Route path={"/projects"} element={<Projects/>}/>
             </Routes>
         </>
     )
