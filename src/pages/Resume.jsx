b91c892ab392e681fa577be85f283b81402a5ecc
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-06 15:53:18 +0530

    Updated resume to view pdf files

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,11 @@
 const Resume = () => {
+    const pdfPath = 'src/assets/resume/Resume-Nureka-Rodrigo.pdf';
 
+    return (
+        <>
+            <embed src={pdfPath} type="application/pdf" width="100%" className="h-screen" />
+        </>
+    )
 }
 
 export default Resume
