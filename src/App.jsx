5da89de372a0bb190ea078b5e91dfbd029391337
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-08 17:07:36 +0530

    Created about page

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Resume from "./pages/Resume.jsx";
 import Contact from "./pages/Contact.jsx";
 import Error404 from "./pages/404.jsx";
 import Snowfall from "react-snowfall";
+import About from "./pages/About.jsx";
 
 function App() {
-
     return (
         <>
             <Snowfall
@@ -16,11 +16,13 @@ function App() {
                 radius={[0.5, 0.5]}
                 speed={[1, 2]}
                 wind={[-0.5, 0.5]}
+                color={"#ffffff"}
                 style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}}
             />
             <div className="bg-white dark:bg-black">
                 <Routes>
                     <Route path={"/"} element={<Home/>}/>
+                    <Route path={"about"} element={<About/>}/>
                     <Route path={"projects"} element={<Projects/>}/>
                     <Route path={"resume"} element={<Resume/>}/>
                     <Route path={"contact"} element={<Contact/>}/>
---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header.jsx";
+
+const About = () => {
+  return(
+      <>
+          <Header/>
+      </>
+  )
+}
+
+export default About;
