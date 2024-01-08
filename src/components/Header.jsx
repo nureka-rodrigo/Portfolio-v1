5c5bfab265b337a8e95d88aa2dae80421f95ae70
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-01-08 16:29:28 +0530

    Moved to more react friendly header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,133 +1,104 @@
-import { Link, useLocation } from "react-router-dom";
-import { useState } from "react";
+import {useState} from 'react'
 import Logo from "/logo-dark.svg";
+import {Dialog, Popover} from '@headlessui/react'
 import ThemeToggle from "./Theme-Toggle.jsx";
-import { IoIosClose, IoIosMenu } from "react-icons/io";
+import {Bars3Icon, XMarkIcon,} from '@heroicons/react/24/outline'
+import {Link, useLocation} from "react-router-dom";
 
-const Header = () => {
+export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
     let pathArray = useLocation().pathname.split("/");
     let lastPart = pathArray[pathArray.length - 1];
 
-    const [menuOpen, setMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
     return (
-        <>
-            <header>
-                <nav
-                    className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-                    aria-label="Global"
-                >
-                    <div className="flex lg:flex-1">
+        <header>
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <div className="flex lg:flex-1">
+                    <Link to="#" className="-m-1.5 p-1.5">
+                        <span className="sr-only">Your Company</span>
+                        <img className="h-8 w-auto dark:invert" src={Logo} alt="Brand"/>
+                    </Link>
+                </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        onClick={() => setMobileMenuOpen(true)}
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon className="h-6 w-6 text-black dark:text-white" aria-hidden="true"/>
+                    </button>
+                </div>
+                <Popover.Group className="hidden lg:flex lg:gap-x-12">
+                    <Link to="/" className={`text-sm font-semibold leading-6 text-gray-900 dark:text-white ${lastPart === "" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}>
+                        Home
+                    </Link>
+                    <Link to="/projects" className={`text-sm font-semibold leading-6 text-gray-900 dark:text-white ${lastPart === "projects" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}>
+                        Projects
+                    </Link>
+                    <Link to="/resume" className={`text-sm font-semibold leading-6 text-gray-900 dark:text-white ${lastPart === "resume" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}>
+                        Resume
+                    </Link>
+                    <Link to="/contact" className={`text-sm font-semibold leading-6 text-gray-900 dark:text-white ${lastPart === "contact" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}>
+                        Contact
+                    </Link>
+                </Popover.Group>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <ThemeToggle/>
+                </div>
+            </nav>
+            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <div className="fixed inset-0 z-10"/>
+                <Dialog.Panel
+                    className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
                         <Link to="/" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
-                            <img
-                                src={Logo}
-                                className="mr-3 h-6 sm:h-9 dark:invert"
-                                alt="Logo"
-                            />
+                            <img className="h-8 w-auto dark:invert" src={Logo} alt="Brand"/>
                         </Link>
-                    </div>
-                    <div className="flex lg:hidden">
+                        <ThemeToggle/>
                         <button
                             type="button"
-                            onClick={toggleMenu}
-                            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            onClick={() => setMobileMenuOpen(false)}
                         >
-                            <span className="sr-only">Open main menu</span>
-                            <IoIosMenu className="h-6 w-6 text-black dark:text-white" />
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6 text-black dark:text-white" aria-hidden="true"/>
                         </button>
                     </div>
-                    <div className="hidden lg:flex lg:gap-x-12">
-                        <Link
-                            to="/"
-                            className={`text-sm font-semibold leading-6 block py-2 pr-4 pl-3 lg:text-primary-700 lg:p-0 text-gray-900 dark:text-white ${lastPart === "" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            to="/projects"
-                            className={`text-sm font-semibold leading-6 block py-2 pr-4 pl-3 lg:text-primary-700 lg:p-0  text-gray-900 dark:text-white ${lastPart === "projects" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
-                        >
-                            Projects
-                        </Link>
-                        <Link
-                            to="/resume"
-                            className={`text-sm font-semibold leading-6 block py-2 pr-4 pl-3 lg:text-primary-700 lg:p-0  text-gray-900 dark:text-white ${lastPart === "resume" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
-                        >
-                            Resume
-                        </Link>
-                        <Link
-                            to="/contact"
-                            className={`text-sm font-semibold leading-6 block py-2 pr-4 pl-3 lg:text-primary-700 lg:p-0  text-gray-900 dark:text-white ${lastPart === "contact" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
-                        >
-                            Contact
-                        </Link>
-                    </div>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <ThemeToggle />
-                    </div>
-                </nav>
-                <div
-                    className={`lg:hidden ${menuOpen ? "block" : "hidden"}`}
-                    role="dialog"
-                    aria-modal="true"
-                >
-                    <div className="fixed inset-0 z-10"></div>
-                    <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:bg-black">
-                        <div className="flex items-center justify-between">
-                            <button
-                                type="button"
-                                onClick={toggleMenu}
-                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                            >
-                                <span className="sr-only">Close menu</span>
-                                <IoIosClose className="h-8 w-8 text-black dark:text-white" />
-                            </button>
-                            <ThemeToggle />
-                        </div>
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
                                 <Link
-                                    to={"/"}
-                                    className={`block py-2 pr-4 pl-3 lg:text-gray-900 lg:p-0 dark:text-white ${lastPart === "" ? "rounded-none border-b-2 border-gray-900 dark:border-white" : ""}`}
+                                    to="/"
+                                    className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 ${lastPart === "" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
                                 >
                                     Home
                                 </Link>
-                            </li>
-                            <li>
                                 <Link
-                                    to={"/projects"}
-                                    className={`block py-2 pr-4 pl-3 lg:text-gray-900 lg:p-0 dark:text-white ${lastPart === "projects" ? "rounded-none border-b-2 border-gray-900 dark:border-white" : ""}`}
+                                    to="/projects"
+                                    className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 ${lastPart === "projects" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
                                 >
                                     Projects
                                 </Link>
-                            </li>
-                            <li>
                                 <Link
-                                    to={"/resume"}
-                                    className={`block py-2 pr-4 pl-3 lg:text-gray-900 lg:p-0 dark:text-white ${lastPart === "resume" ? "rounded-none border-b-2 border-gray-900 dark:border-white" : ""}`}
+                                    to="/resume"
+                                    className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 ${lastPart === "resume" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
                                 >
                                     Resume
                                 </Link>
-                            </li>
-                            <li>
                                 <Link
-                                    to={"/contact"}
-                                    className={`block py-2 pr-4 pl-3 lg:text-primary-700 lg:p-0  text-gray-900 dark:text-white ${lastPart === "contact" ? "rounded-none border-b-2 border-white" : ""}`}
+                                    to="/contact"
+                                    className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 dark:text-white hover:bg-gray-50 ${lastPart === "contact" ? "rounded-none border-b-2 border-black dark:border-white" : ""}`}
                                 >
                                     Contact
                                 </Link>
-                            </li>
-                        </ul>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </header>
-        </>
-    );
-};
-
-export default Header;
+                </Dialog.Panel>
+            </Dialog>
+        </header>
+    )
+}
