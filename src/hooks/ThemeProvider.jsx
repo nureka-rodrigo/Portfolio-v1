f3f7bf2f70b6f9195773243fc2e60a5c5978a36b
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-03-03 19:44:42 +0530

    Made background color of page transition animation change dynamically with the theme

--- src/hooks/ThemeProvider.jsx
+++ src/hooks/ThemeProvider.jsx
@@ -1,7 +1,7 @@
 import {createContext, useContext, useEffect, useState} from "react";
 import PropTypes from "prop-types";
 
-const ThemeContext = createContext();
+const ThemeContext = createContext(undefined);
 
 export const useTheme = () => {
     return useContext(ThemeContext);
@@ -20,8 +20,10 @@ export const ThemeProvider = ({children}) => {
                     window.matchMedia("(prefers-color-scheme: dark)").matches)
             ) {
                 document.documentElement.classList.add("dark");
+                document.documentElement.style.setProperty('--bg-color', '#fff');
             } else {
                 document.documentElement.classList.remove("dark");
+                document.documentElement.style.setProperty('--bg-color', '#000');
             }
         };
 
