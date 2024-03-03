c6a2867b6331ed04ad2db9fa446bbd10c2c1f928
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-03-03 20:11:30 +0530

    Minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -72,9 +72,9 @@ const Header = () => {
                 <div className="fixed inset-0 z-10"/>
                 <Dialog.Panel
                     className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                    <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-between pulse-item">
                         <Link to="/" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                            <span className="sr-only">Nureka Rodrigo</span>
                             <img className="h-8 w-auto dark:invert" src={Logo} alt="Brand"/>
                         </Link>
                         <ThemeToggle/>
